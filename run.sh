bacde18d2565ce2b1947971cea80c89ec2dcf618
Saivamshi <dsaivamshi123@gmail.com>
2023-04-20 11:37:26 +0530

    updated sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-@ECHO ON
-    call python manage.py makemigrations
-    call python manage.py migrate
-    call gunicorn -w 1 portfolio.wsgi:application
+#!/bin/bash
+python manage.py makemigrations
+python manage.py migrate
+gunicorn -w 1 portfolio.wsgi:application
